43aced540f421933b1167ea2473ec0f9e7488da5
andrade-lopes <jlopes@byupathway.edu>
2026-06-04 22:26:28 -0100

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("Project successfully initiated!");
+const productContainer = document.querySelector(".products");
+
+async function loadProducts() {
+    try {
+        const response = await fetch("/data/products.json");
+        const products = await response.json();
+
+        displayProducts(products);
+    } catch (error) {
+        console.error("Error loading products:", error);
+    }
+}
+
+function displayProducts(products) {
+    products.forEach(product => {
+        const card = document.createElement("div");
+        card.classList.add("product-card");
+
+        card.innerHTML = `
+      <img src="${product.image}" alt="${product.name}">
+      <h3>${product.name}</h3>
+      <p>$${product.price}</p>
+    `;
+
+        productContainer.appendChild(card);
+    });
+}
+
+loadProducts();
